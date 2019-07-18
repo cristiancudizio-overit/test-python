76ed80cac438a8ab392db533b20eb37b162b45b8
Cristian Cudizio <cristian.cudizio@overit.it>
2019-07-18 12:21:57 +0200

    aggiornamenti a excel magazzini e varie

---
+++ test-python/export_excel_magazzini.cmd
@@ -0,0 +1,5 @@
+call c:\oracle\instantclient_18_3\conf\vars.cmd
+set path=c:\oracle\instantclient_18_3;%PATH%
+set ORACLE_HOME=C:\ORACLE\instantclient_18_3
+cd \Development-cri\test-python\test-python
+python export_excel_magazzini.py
---
+++ test-python/monitor_camuzzi2.cmd
@@ -0,0 +1,4 @@
+call c:\oracle\instantclient_18_3\conf\vars.cmd
+set path=c:\oracle\instantclient_18_3;%PATH%
+set ORACLE_HOME=C:\ORACLE\instantclient_18_3
+start cmd /C python monitor_camuzzi2.py
